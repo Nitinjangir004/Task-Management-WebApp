83526b80f9a225181eb9d32d2bd6f84ffa6b389f
Nitin Jangir <nitinjangir004@gmail.com>
2026-05-03 21:21:06 +0000

    adding signup routes

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-
+import { config } from "dotenv";
+config();
+const mongourl= process.env.MONGODB_URL;
 export default async function mongodb(){
     try{
-        await mongoose.connect("mongodb+srv://nitinjangir004:DbNikkulinkblue@cluster0.jqwg5.mongodb.net/Task-Management-Webapp"
-    ).then(()=>{console.log("Successfully connected Database")})}
+        await mongoose.connect(mongourl).then(()=>{console.log("Successfully connected Database")})}
     catch(error){
         console.log("error while connecting db " +error);
     }
