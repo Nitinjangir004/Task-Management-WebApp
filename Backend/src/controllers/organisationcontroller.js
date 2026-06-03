2c29c1c936815365dcebe9bade83e28280ee32ff
Nitin Jangir <nitinjangir004@gmail.com>
2026-06-03 17:38:36 +0000

    create org. controller

--- Backend/src/controllers/organisationcontroller.js
+++ Backend/src/controllers/organisationcontroller.js
@@ -1,4 +1,39 @@
+import Organisation from "../model/organisation.js";
+import User from "../model/user.js";
+
 // Create organisation
 export const createOrg = async (req,res)=>{
-    
+    try {
+        const userid = req.user.id;
+        const {name,description,member}= req.body;
+        if(!userid){
+            return res.status(400).json({
+                success:false,
+                message:'User id not found'
+            })
+        }
+        const User = await User.findone(userid);
+        if(!name){
+            return res.status(400).json({
+                success:false,
+                message:'name not found'
+            })    
+        }
+        const Organisation =await Organisation.create({
+            name:name,
+            description:description,
+            member:member,
+            createdBy:userid
+        })
+        return res.status(200).json({
+            success:true,
+            message:"Orignation is Created Successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Server error",
+            error:error.message
+        })
+    }
 }
